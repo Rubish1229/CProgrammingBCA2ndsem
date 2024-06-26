63bacd1c0a5a37f379c1968939f1429cc08f0f04
Rubish1229 <karmacharyarubish5@gmail.com>
2024-06-26 12:08:34 +0545

    Modified by Rubish Karmacharya on June 26 at 12:08 PM

---
+++ calculator version.1.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+	int firstnum,secondnum;
+	printf("Enter any two numbers:\n");
+	scanf("%d %d",&firstnum,&secondnum);
+	//subtraction of two numbers
+		int sub=firstnum-secondnum;
+		printf("The subtracted value is:%d",sub);
+		
+	//addition of two numbers
+	int sum=firstnum+secondnum;
+		printf("\nThe subtracted value is:%d",sum);
+	
+	//multiplication of two numbers	
+		int mult=firstnum*secondnum;
+		printf("\nThe multiplication is:%d",mult);
+		
+	//division of two numbers
+		int divide=firstnum/secondnum;
+		printf("\nThe division is:%d",divide);
+		return 0;
+}
+
+
+
+		
+
