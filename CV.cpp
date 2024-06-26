86ea61713df8c554e953781729c23b4d1df53693
Rubish1229 <karmacharyarubish5@gmail.com>
2024-06-26 12:42:57 +0545

    Updated by Rubish Karmacharya on June 26 at 12:42 pm

---
+++ CV.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+// name,roll,faculty,age,email,section
+int roll,age;
+char section;
+char name[25],faculty[25],email[50];
+
+printf("Enter your Name:");
+scanf("%s",&name);
+printf("Enter Roll:");
+scanf("%d",&roll);
+printf("Enter faculty:");
+scanf("%s",&faculty);
+printf("Enter age:");
+scanf("%d",&age);
+printf("Enter email:");
+scanf("%s",&email);
+printf("Enter section:");
+scanf(" %c",&section);
+
+
+
+}
