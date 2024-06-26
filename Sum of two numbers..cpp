bb945705afa09f5aaeb8320983c8ae20777978d6
Rubish Karmacharya <163443549+Rubish1229@users.noreply.github.com>
2024-06-26 21:31:25 +0545

    Delete Sum of two numbers..cpp

--- Sum of two numbers..cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-	int firstnumber,secondnumber,sum;
-		printf("enter two numbers:\n");
-		scanf("%d %d",&firstnumber,&secondnumber);
-		sum=firstnumber+secondnumber;
-		printf("The sum of two number is:%d",sum);
-	return 0;
-}
