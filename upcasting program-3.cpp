8aa11e48115a099e200b65ec856fc6202bbcc15b
Rubish1229 <karmacharyarubish5@gmail.com>
2024-07-01 22:41:25 +0545

    Updated by Rubish karmacharya on July 1 at 10:41 pm

---
+++ upcasting program-3.cpp
@@ -0,0 +1,10 @@
+//Upcasting.....
+#include<stdio.h>
+int main(){
+	int first_num=20;
+	int second_num=7;
+	float division = (float) first_num/second_num;
+		printf("The division is:%f",division);
+		
+	return 0;
+}
