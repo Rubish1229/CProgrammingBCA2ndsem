f312fee13951131a6c9783569b058027246376e2
Rubish1229 <karmacharyarubish5@gmail.com>
2024-07-15 11:00:16 +0545

    Updated by Rubish Karmacharya on Junly-17 at 11:00 am

---
+++ array to check number.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int num[5]={33,69,44,3,91};
+	int i,count=0;
+	
+	
+	for(i=0;i<5;i++)
+	{
+		if(num[i] == 3){
+			printf("The number is:%d",i);
+			
+		}
+		
+	}
+}
